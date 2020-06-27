84c6a3cc26e0fadbcc6553155015d40c66c0d2c9
Sandro Hallado <sandrohallado@yahoo.com>
2020-06-26 19:34:57 -0700

    Explain some simple schema structures in mongodb

---
+++ practice.js
@@ -0,0 +1,29 @@
+// explain what type of schema is this ? 
+// Essentially this approach makes it so the data structures are the same just different values
+db.products.insertOne({
+  name: 'A book',
+  price: 12.99
+});
+
+db.products.insertOne({
+  name: 'A T-Shirt',
+  price: 20.99
+});
+
+// explain what type of schema is this?
+// Essentially the same data structure just with different values but some will have extra fields such as the last inserted document in the collection
+db.products.insertOne({
+  name: 'A book',
+  price: 12.99
+});
+db.products.insertOne({
+  name: 'A T-Shirt',
+  price: 20.99
+});
+db.products.insertOne({
+  name: 'A Computer',
+  price: 12.99,
+  details: {
+    cpu: 'Intel i7 8770'
+  }
+});
